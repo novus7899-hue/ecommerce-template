cf8a4dbde2fbaea42033a9a287be4396f61e414d
anonymous0909-cpu <mokshsharma9008.business@gmail.com>
2025-09-16 21:36:59 +0530

    first commit

---
+++ scripts/blogpage-scpipt.js
@@ -0,0 +1,35 @@
+let one = document.querySelector(".blog-extra1")
+let two = document.querySelector(".blog-extra2")
+let three = document.querySelector(".blog-extra3")
+let four = document.querySelector(".blog-extra4")
+let link1 = document.querySelector(".blog-link1")
+let link2 = document.querySelector(".blog-link2")
+let link3 = document.querySelector(".blog-link3")
+let link4 = document.querySelector(".blog-link4")
+function reading1(one) {
+    one.innerHTML = `When it comes to wardrobe essentials, the cotton-jersey zip-up hoodie <br>
+     stands out as a timeless piece. Loved for its versatility, this hoodie blends <br> comfort, functionality, and casual style—making it a staple for every season.<br>
+
+Crafted from soft cotton-jersey fabric, it offers breathability and durability,<br> perfect for both lounging at home and heading out for errands. The zip-up <br> design adds convenience, allowing you to layer it easily over a t-shirt or under <br> a jacket. With a relaxed fit and minimal style, it pairs effortlessly with jeans, <br> joggers, or even shorts.
+
+`
+    
+    link1.innerHTML = "";
+}
+function reading2(two) {
+   
+    
+    two.innerHTML = " Cotton tops provide breathability for daily wear, while chiffon or silk <br> options add a touch of sophistication for special events. Cropped styles <br> pair perfectly with high-waist jeans, and flowy tunics offer effortless charm. <br> Colors, prints, and embellishments allow girls to express individuality. <br> Whether layered with jackets or worn solo, a well-chosen top enhances <br> confidence and completes any look."
+
+    link2.innerHTML = "";
+}
+function reading3(three) {
+    three.innerHTML = "sdsdsdjsjskdjsdkjskdjskdjksjdjskdskjdksjdksjkdjsk"
+
+    link3.innerHTML = "";
+}
+function reading4(four,link) {
+    four.innerHTML = "mokssdjsdjskdjksdjkj "
+
+    link4.innerHTML = "";
+}
---
+++ scripts/script-news.js
@@ -0,0 +1,10 @@
+// newsletter EMAIL 
+
+function news() {
+    let var1 = document.querySelector(".input-button").innerHTML="";
+    document.querySelector(".news-letter-input-p").innerHTML = "Thanks For Submitting. We Will reach to you soon.... "
+    
+    
+}
+
+// newsletter EMAIL DONE
